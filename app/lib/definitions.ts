ae2b2f27a9699799ee59322df533d028626d5722
Marcelo Pinheiro <mpinheir@gmail.com>
2024-02-16 17:23:34 -0800

    Add UI refinements

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -2,6 +2,17 @@
 // It describes the shape of the data, and what data type each property should accept.
 // For simplicity of teaching, we're manually defining these types.
 // However, these types are generated automatically if you're using an ORM such as Prisma.
+export type Estagio = {
+  id: string;
+  name: string;
+  empresa: string;
+  requisitos: string;
+  estado: string;
+  cidade: string;
+  contato: string;
+  salario: string;
+};
+
 export type User = {
   id: string;
   name: string;
--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,9 @@ export default function Page() {
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
+        <div
+  className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent"
+/>
           <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
             <strong>Quer estagiar ou tem um estágio para oferecer?</strong>
           </p>
