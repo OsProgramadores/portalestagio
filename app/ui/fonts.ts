798cccf36cbedd1f1440a32ed2704b61dc62379f
Marcelo Pinheiro <mpinheir@gmail.com>
2024-02-20 21:38:22 -0800

    Resolve problema com fonte Lusitana.

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,3 +1,7 @@
-import { Inter } from 'next/font/google';
+import { Inter, Lusitana } from 'next/font/google';
  
 export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+    });
