ea1a4dc636a131d82c0e703cc59e4c877101096d
Marcelo Pinheiro <mpinheir@gmail.com>
2024-02-23 20:57:48 -0800

    Reduz altura da barra do topo da página.

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import EstagioLogo from '@/app/ui/estagio-logo';
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
+      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-26">
         {<EstagioLogo />}
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
