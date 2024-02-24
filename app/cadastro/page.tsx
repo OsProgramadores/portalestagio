5d6cbcd7327dec0591b274c5f5d7d6584a8bac26
AndreLuizpDev <andreldsantosp@gmail.com>
2024-02-24 10:28:07 -0300

    Adicionado import do logo tela cadastro

--- app/cadastro/page.tsx
+++ app/cadastro/page.tsx
@@ -1,3 +1,14 @@
-export default function Page() {
-  return <p>Página para cadastrar vagas de estágio</p>;
+import EstagioLogo from '@/app/ui/estagio-logo';
+
+export default function Page() {  
+  return (
+    <main className="flex min-h-screen flex-col p-6">
+      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-26">
+        {<EstagioLogo />}
+      </div>
+      <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
+        <p>Página para cadastrar vagas de estágio</p>
+      </div>
+    </main>
+  );
 }
