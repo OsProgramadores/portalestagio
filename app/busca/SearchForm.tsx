0306e54e764afb03f4f121f6c9a64a80c30f3e74
Jose Ulisses <joseulisses03@gmail.com>
2024-02-25 21:57:30 -0300

    adicionando campo busca por regime

--- app/busca/SearchForm.tsx
+++ app/busca/SearchForm.tsx
@@ -1,16 +1,21 @@
+
 export default function SearchForm() {
     return (
         <form>
             <div className="flex flex-col gap-6 rounded-lg bg-gray-50 px-6 py-10 md:px-20">
                 <div className="flex flex-col">
-                    <label>Buscar por empresa<span className="text-red-500">*</span></label>
+                    <label>Buscar por empresa</label>
                     <input type="text" name="empresa" required />
                 </div>
             </div>
             <div className="flex flex-col gap-6 rounded-lg bg-gray-50 px-6 py-10 md:px-20">
                 <div className="flex flex-col">
-                    <label>Buscar por estado<span className="text-red-500">*</span></label>
-                    <input type="text"></input>"
+                    <label>Buscar por regime</label>
+                    <select name="ufs">
+                        <option value="remoto">Remoto</option>
+                        <option value="hibrido">Hibrido</option>
+                        <option value="presencial">Presencial</option>
+                    </select>
                 </div>
             </div>     
         </form>
