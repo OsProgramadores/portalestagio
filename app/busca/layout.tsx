eaebdd86114f897576b008479087154e6a7eda87
Jose Ulisses <joseulisses03@gmail.com>
2024-02-25 21:08:24 -0300

    Removendo menu da landing page

---
+++ app/busca/layout.tsx
@@ -0,0 +1,15 @@
+import TopNav from '@/app/ui/cadastro/topnav';
+
+export default function BuscaLayout({
+    children,}: {
+        children: React.ReactNode
+    }) {
+        return (
+        <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+            <div className="w-full flex-none md:w-20">
+              <TopNav />
+            </div>
+            <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+        </div>
+        )
+    }
