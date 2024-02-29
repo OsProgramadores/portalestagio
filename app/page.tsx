de0e1a776706f95a877cd26976a36670d3f71668
Marcelo Pinheiro <mpinheir@gmail.com>
2024-02-28 21:26:14 -0800

    Cria esboço da home page.

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,24 @@ import SearchBox from './components/SearchBox';
 import Button from './components/Button';
 export default function Page() {
   return (
-    <div className="flex space-x-4 mt-2 ml-2 mr-2">
-      <div className='flex-none'><PortalEstagioLogo /></div>
-      <div className='grow'><SearchBox /></div>
-      <div className='flex-none'><Button /></div>
+    <div className='bg-white flex flex-col h-screen'>
+      <div className='=flex-none'> 
+        <div className="flex space-x-4 mt-2 ml-2 mr-2">
+          <div className='flex-none'><PortalEstagioLogo /></div>
+          <div className='grow'><SearchBox /></div>
+          <div className='flex-none'><Button /></div>
+        </div>
+      </div>
+      <div className='flex-grow bg-sky-50 mt-2 ml-2 mr-2' >
+      <div className="flex flex-col items-center justify-center" style={{ height: '100%' }}>
+          area reservada para os menus e resultado de busca
+        </div>
+      </div>
+      <div className='flex-none'>
+        <div className='flex justify-center grow mt-2 ml-2 mr-2'>
+          Portal Estágio - 2024
+        </div>
+      </div>
     </div>
   );
 }
