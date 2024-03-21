f594aa5e3f3e69b6e18b55cdf1bb3dccdfddb131
Angela Talyta <tatazinhagms2@gmail.com>
2024-03-21 10:06:19 -0300

    Alteração no titulo da logo

--- app/ui/cadastro/nav-links.tsx
+++ app/ui/cadastro/nav-links.tsx
@@ -21,9 +21,9 @@ export default function NavLinks() {
           <a
             key={link.name}
             href={link.href}
-            className="flex items-center gap-2 h-[48px] grow rounded-md p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600"
+            className="flex items-center gap-2 h-[52px] grow rounded-md px-0 text-sm font-medium hover:bg-sky-100 hover:text-blue-600"
           >
-            <LinkIcon className="w-6" />
+            <LinkIcon className="w-7 mt-8 " />
             <p className="hidden md:block">{link.name}</p>
           </a>
         );
