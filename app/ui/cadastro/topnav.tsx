bb1c34b4efb2b931a0e381e66493ac842d03b55d
Angela Talyta <87730446+tatazinhaang@users.noreply.github.com>
2024-03-22 01:32:23 -0300

    Alteração no titulo da logo (#102)

--- app/ui/cadastro/topnav.tsx
+++ app/ui/cadastro/topnav.tsx
@@ -2,7 +2,7 @@ import NavLinks from '@/app/ui/cadastro/nav-links';
 
 export default function TopNav() {
   return (
-    <div className="flex h-full flex-row px-3 py-4 md:px-2">
+    <div className="flex h-full flex-row px-0 py-4 md:px-2">
         <NavLinks />
     </div>
   );
--- app/ui/estagio-logo.tsx
+++ app/ui/estagio-logo.tsx
@@ -4,10 +4,10 @@ import { lusitana } from '@/app/ui/fonts';
 export default function EstagioLogo() {
   return (
     <div
-      className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
+      className={`${lusitana.className} sm:ml-7 flex flex-row items-center leading-none text-white`}
     >
       <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[44px]">Portal Estágio</p>
+      <p className="text-44px text-sm-5rem sm:text-base md:text-xl lg:text-2xl">Portal Estágio</p>
     </div>
   );
 }
