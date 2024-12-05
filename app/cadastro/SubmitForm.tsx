f2b3aacf160bd86f583d5f2f49e1cd1888e5d212
AlexHorta <buey.horta@gmail.com>
2024-12-04 23:31:27 -0300

    Usando useId nos formulários da forma adequada

--- app/cadastro/SubmitForm.tsx
+++ app/cadastro/SubmitForm.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { FormEvent } from 'react';
+import { FormEvent, useId } from 'react';
 
 export default function SubmitForm() {
   async function onSubmit(event: FormEvent<HTMLFormElement>) {
@@ -17,9 +17,10 @@ export default function SubmitForm() {
     // ...
   }
 
-  function Required_input({inputLabel, inputId, inputName}) {
+  function Required_input({inputLabel, inputName}) {
+        const inputId = useId();
         return <div className="flex flex-col">
-          <label class="after:content-['*'] after:ml-0.5 after:text-red-500" for="{inputId}">
+          <label className="after:content-['*'] after:ml-0.5 after:text-red-500" htmlFor="{inputId}">
             {inputLabel}</label>
           <input id="{inputId}" type="text" name="{inputName}" required />
         </div>;
@@ -29,17 +30,17 @@ export default function SubmitForm() {
     <form onSubmit={onSubmit}>
       <div className="flex flex-col gap-6 rounded-lg bg-gray-50 px-6 py-10 md:px-20"> 
         {[
-            ['Empresa', 'empresa-input', 'empresa'],
-            ['CNPJ da Empresa', 'cnpj-input', 'CNPJ'],
-            ['Descrição da Vaga', 'descricao-input', 'descricao'],
-            ['Cidade', 'cidade-input', 'cidade'],
-            ['Estado', 'estado-input', 'estado']
-        ].map(([label, id, name]) => (
-            <Required_input inputLabel={label} inputId={id} inputName={name} />
+            ['Empresa', 'empresa'],
+            ['CNPJ da Empresa', 'CNPJ'],
+            ['Descrição da Vaga', 'descricao'],
+            ['Cidade', 'cidade'],
+            ['Estado', 'estado']
+        ].map(([label, name]) => (
+            <Required_input inputLabel={label} inputName={name} />
         ))}
 
         <div className="flex flex-col">
-            <label for="modalidade-select" class="after:content-['*'] after:ml-0.5 after:text-red-500">
+            <label htmlFor="modalidade-select" className="after:content-['*'] after:ml-0.5 after:text-red-500">
             Modalidade do Estágio</label>
             <select id="modalidade-select" name="Modalidade" required>
                 <option value="Presencial">Presencial</option>
@@ -49,15 +50,15 @@ export default function SubmitForm() {
         </div>
 
         {[
-            ['Carga Horária', 'carga-input', 'CargaHoraria'],
-            ['Remuneração', 'remuneracao-input', 'Remuneracao'],
-            ['Benefícios', 'beneficios-input', 'Beneficios'],
-            ['Responsabilidades', 'responsabilidades-input', 'Responsabilidades'],
-            ['Responsável pelo Estágio', 'responsavel-input', 'ResponsavelEstagio'],
-            ['Email do Responsável', 'emailresponsavel-input', 'EmailResponsavel'],
-            ['Como se Candidatar?', 'comocandidatar-input', 'ComoCandidatar'],
-        ].map(([label, id, name]) => (
-            <Required_input inputLabel={label} inputId={id} inputName={name} />
+            ['Carga Horária', 'CargaHoraria'],
+            ['Remuneração', 'Remuneracao'],
+            ['Benefícios', 'Beneficios'],
+            ['Responsabilidades', 'Responsabilidades'],
+            ['Responsável pelo Estágio', 'ResponsavelEstagio'],
+            ['Email do Responsável', 'EmailResponsavel'],
+            ['Como se Candidatar?', 'ComoCandidatar'],
+        ].map(([label, name]) => (
+            <Required_input inputLabel={label} inputName={name} />
         ))}
 
         <div className="flex h-10 items-center justify-center rounded-lg bg-blue-500 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500 active:bg-blue-600 aria-disabled:cursor-not-allowed aria-disabled:opacity-50">
